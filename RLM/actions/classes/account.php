3ab0215115e4e730b3c4ca843280e84251cad7fb
Kalmani <kolosowski.florent@gmail.com>
2015-02-22 17:51:56 +0100

    update settings inputs

--- RLM/actions/classes/account.php
+++ RLM/actions/classes/account.php
@@ -15,6 +15,12 @@ class Account {
       case 'login' :
         echo json_encode($this->login());
         break;
+      case 'change_login' :
+        echo json_encode($this->change_login());
+        break;
+      case 'change_email' :
+        echo json_encode($this->change_email());
+        break;
     }
   }
 
@@ -53,12 +59,117 @@ class Account {
     );
   }
 
+  private function change_login() {
+    $user = json_decode($_COOKIE['RLM_user'], true);
+
+    $user_log = $this->params['identifiant'];
+    $old_password = $this->params['old_password'];
+    $new_password = $this->params['new_password'];
+    $new_password_conf = $this->params['new_password_conf'];
+
+    if (empty($this->params['identifiant']))
+      return array(
+        'error' => array(
+          'message' => "Merci de saisir un identifiant correct"
+        )
+      );
+
+    if ($user['user_pass'] !== $old_password
+        && $user['user_pass'] !== md5($old_password))
+      return array(
+        'error' => array(
+          'message' => "Mot de passe actuel incorrect"
+        )
+      );
+
+    if (empty($new_password) && empty($new_password_conf)) {
+      $req = "UPDATE larmes_identifiants SET user_log = '".$user_log."' WHERE user_id = ".$user['user_id'];
+      $res = $this->mysqli->query($req);
+      $user['user_log'] = $user_log;
+      $_COOKIE['RLM_user'] = json_encode($user);
+      return array(
+        'success' => array(
+          'message' => 'Identifiant modifié avec succès'
+        ),
+        'error_case' => array(
+          'message' => "Une erreur est survenue, contacter un administrateur"
+        ),
+        'datas' => array(
+          'user_log' => $user_log
+        )
+      );
+    }
+
+
+    if ((!empty($new_password) || !empty($new_password_conf))
+        && ($new_password !== $new_password_conf))
+      return array(
+        'error' => array(
+          'message' => "Merci de saisir des mots de passe identiques"
+        )
+      );
+
+    if ((!empty($new_password) || !empty($new_password_conf))
+        && ($new_password === $new_password_conf)) {
+      $req = "UPDATE larmes_identifiants SET user_log = '".$user_log."', user_pass = '".md5($new_password)."' WHERE user_id = ".$user['user_id'];
+      $res = $this->mysqli->query($req);
+      return array(
+        'success' => array(
+          'message' => 'Identifiant et mot de passe modifiés avec succès'
+        ),
+        'error_case' => array(
+          'message' => "Une erreur est survenue, contacter un administrateur"
+        ),
+        'datas' => array(
+          'user_log' => $user_log,
+          'user_pass' => md5($new_password)
+        )
+      );
+    }
+    return array(
+      'error' => array(
+        'message' => "Vous êtes tombé sur une exception, merci de contacter un administrateur en précisant votre parcour"
+      )
+    );
+  }
+
+  private function change_email() {
+    $user = json_decode($_COOKIE['RLM_user'], true);
+    $user_mail = $this->params['email'];
+    if (empty($user_mail) || !$this->is_email($user_mail)) {
+      return array(
+        'error' => array(
+          'message' => "Email incorrect"
+        )
+      );
+    } else {
+      $req = "UPDATE larmes_identifiants SET user_mail = '".$user_mail."' WHERE user_id = ".$user['user_id'];
+      $res = $this->mysqli->query($req);
+      $user['user_mail'] = $user_mail;
+      $_COOKIE['RLM_user'] = json_encode($user);
+      return array(
+        'success' => array(
+          'message' => 'Adresse email modifiée avec succès'
+        ),
+        'error_case' => array(
+          'message' => "Une erreur est survenue, contacter un administrateur"
+        ),
+        'datas' => array(
+          'user_mail' => $user_mail
+        )
+      );
+    }
+
+  }
+
   private function isValidMd5($md5 ='') {
     return preg_match('/^[a-f0-9]{32}$/', $md5);
   }
-}
 
-//echo isValidMd5('5d41402abc4b2a76b9719d911017c592');
+  private function is_email($email) {
+    return filter_var($email, FILTER_VALIDATE_EMAIL);
+  }
+}
 
 $mysqli = new mysqli($host, $user, $pass, $db);
 $account = new Account($action, $mysqli, $params);
--- RLM/application/globales/rubrics/settings.js
+++ RLM/application/globales/rubrics/settings.js
@@ -7,8 +7,11 @@ ScreenEvents.actions.settings = {
     this.app = app;
 
     switch(this.id) {
-      case 'btn_test' : 
-        this.test_func();
+      case 'change_login_pass' :
+        this.change_login_pass();
+        break;
+      case 'change_email' :
+        this.change_email();
         break;
       default :
         console.info('No bind on button ' + this.id);
@@ -16,8 +19,68 @@ ScreenEvents.actions.settings = {
     }
   },
 
-  test_func : function() {
-    console.log(this.rubric);
+  change_login_pass : function() {
+    var ids = [
+          'identifiant',
+          'old_password',
+          'new_password',
+          'new_password_conf'
+        ],
+        values = {};
+
+    Array.each(ids, function(id) {
+      values[id] = document.id(id).get('value').trim();
+    });
+
+    var options = {
+          'success' : this.callback_change_login.bind(this)
+        },
+        params = values;
+    this.app.ask_server('account', 'change_login', params, options);
   },
+
+  callback_change_login : function(response) {
+    var response = JSON.parse(response);
+    if (response.error) {
+      this.app.alertMessage('error', response.error, document.id('cell_settings_panels'));
+    } else if (response.warning) {
+      this.app.alertMessage('warning', response.warning, document.id('cell_settings_panels'));
+    } else if (response.success) {
+      this.app.sess.update_value('user_log', response.datas.user_log);
+      if (response.datas.user_pass)
+        this.app.sess.update_value('user_pass', response.datas.user_pass);
+      this.app.alertMessage('success', response.success, document.id('cell_settings_panels'));
+      setTimeout(function () {
+        this.app.SCS.switchRubric('HOME');
+      }.bind(this), 1500);
+    }
+  },
+
+  change_email : function() {
+    var email = document.id('email').get('value').trim();
+    var options = {
+          'success' : this.callback_change_email.bind(this)
+        },
+        params = {'email' : email};
+    this.app.ask_server('account', 'change_email', params, options);
+  },
+
+  callback_change_email : function(response) {
+    var response = JSON.parse(response);
+    console.log(response);
+    if (response.error) {
+      this.app.alertMessage('error', response.error, document.id('cell_settings_panels'));
+    } else if (response.warning) {
+      this.app.alertMessage('warning', response.warning, document.id('cell_settings_panels'));
+    } else if (response.success) {
+      this.app.sess.update_value('user_mail', response.datas.user_mail);
+      if (response.datas.user_pass)
+        this.app.sess.update_value('user_pass', response.datas.user_pass);
+      this.app.alertMessage('success', response.success, document.id('cell_settings_panels'));
+      setTimeout(function () {
+        this.app.SCS.switchRubric('HOME');
+      }.bind(this), 1500);
+    }
+  }
 
 };
