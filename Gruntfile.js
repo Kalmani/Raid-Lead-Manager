12c36777d0879c53fa70e94a1b5c5992a2a034ed
Kalmani <kolosowski.florent@gmail.com>
2015-02-22 12:49:41 +0100

    prepare dev/prod modes

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,27 +2,13 @@ module.exports = function(grunt) {
   var path = require("path");
       dev_mode   = (grunt.cli.tasks[0] != 'distribute'),
       deploy_dir = dev_mode ? 'RLM' : 'server';
-
+  grunt.dev_mode = dev_mode;
   grunt.initConfig({
     pkg: require('./package.json'),
     manifest : require('./manifest.json'),
     deploy_dir : grunt.option('deploy_dir') || deploy_dir,
-    absolute_root : path.resolve(__dirname),
-    'ftp-deploy': {
-      build: {
-        auth: {
-          host: 'server_ip',
-          port: 21,
-          authKey: 'key1'
-        },
-        src: 'server/',
-        dest: '/path_to_install/',
-        exclusions: []
-      }
-    }
+    absolute_root : path.resolve(__dirname)
   });
-
-  grunt.dev_mode = (grunt.config.get('manifest').dev_mode) ? true : false;
 
   grunt.file.expand({filter:'isDirectory'}, 'grunt/**').forEach(grunt.loadTasks);
   grunt.log.writeln("Working in '%s'", grunt.config('deploy_dir'));
