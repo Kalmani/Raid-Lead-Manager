a1cf3df48d209e869fc8bcda4630861730795699
Kalmani <kolosowski.florent@gmail.com>
2015-02-24 22:34:46 +0100

    add wowhead notices

--- RLM/application/init.js
+++ RLM/application/init.js
@@ -1,4 +1,5 @@
 window.addEvent('domready', function() {
+  var wowhead_tooltips = { "colorlinks": false, "iconizelinks": false, "renamelinks": false }
   window.RLM = new RaidLeadManager();
   RLM.init();
 });
