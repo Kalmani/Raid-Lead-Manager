7b4eb113c1ce7faef95dd860389e83407df3348f
kalmani <kolosowski.florent@gmail.com>
2016-01-30 19:40:56 +0100

    loop import items

--- RLM/application/globales/rubrics/admin/import.js
+++ RLM/application/globales/rubrics/admin/import.js
@@ -27,6 +27,7 @@ module.exports = {
     if (item_id != parseInt(item_id) || !item_id)
       return;
 
+    self.total = 0;
     var options = {
           'success' : self.loop_import_item.bind(this)
         },
@@ -42,11 +43,16 @@ module.exports = {
         data = JSON.parse(data),
         next_id = data.next_id;
 
-    if (data.no_item) {
-      console.error('No item for id ', data.no_item);
-    } else {
-      console.log('Item found for id ', data.item_id);
-    }
+    self.total++;
+
+    var total_container = document.id('total_container'),
+        current_status_container = document.id('current_status_container'),
+        total_dom = new Element('span', {'html' : 'Total items scaned : ' + self.total}),
+        current_dom = new Element('span', {'html' : ((data.no_item) ? 'No item for id ' + data.no_item : 'Item found for id ' + data.item_id)  + '<br />Next id : ' + next_id});
+    
+    total_dom.inject(total_container.empty());
+    current_dom.inject(current_status_container.empty());
+
     var options = {
           'success' : self.loop_import_item.bind(this)
         },
